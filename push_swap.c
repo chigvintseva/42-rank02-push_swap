21fdcefa4ec733bc303aea300b964457a3ebbf5f
chigvintseva <al.chigvintseva@gmail.com>
2025-09-19 18:00:17 +0200

    i hope length of the stack function is correct even though i dont even have yet the stack init functions

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 16:58:01 by achigvin          #+#    #+#             */
-/*   Updated: 2025/09/19 17:47:59 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/09/19 17:59:38 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 int	main(int argc, char **argv)
 {
 	t_stack	*a;
+	t_stack	*b;
 
 	if (argc == 1)
 		return (write(2, "Error\n", 7), 0);
 	a = parse_input(argv[i], a);
 	if (a == NULL)
 		return (write(2, "Error\n", 7), 0);
-	
-	
+	b = init_stack(stack_len(a), b);
+	// is stack b initialised separately using length of stack a? 
+	// r pointers repeated in calls? like will they change aning or should i start sending double ptrs to not mess up with them?
 	return (0);
 }
--- stack_init_free.c
+++ stack_init_free.c
@@ -6,13 +6,31 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 17:42:10 by achigvin          #+#    #+#             */
-/*   Updated: 2025/09/19 17:49:52 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/09/19 17:58:33 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.c"
 
 void	add_node(t_stack *head, t_stack *new)
+{
+	
+}
+
+size_t	stack_len(t_stack *a)
+{
+	size_t	i;
+
+	i = 0;
+	while (a->next != NULL)
+	{
+		a = a->next;
+		i++;
+	}
+	return (i);
+}
+
+t_stack	*init_stack(t_stack	*a, t_stack	*b_to_init)
 {
 	
 }
