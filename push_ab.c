0dd1379ee332bf4e0f1593e73bc15df8dc9048f5
chigvintseva <al.chigvintseva@gmail.com>
2025-10-30 18:05:07 +0100

    push ab and swap ab instructions

--- push_ab.c
+++ push_ab.c
@@ -6,7 +6,26 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 17:27:41 by achigvin          #+#    #+#             */
-/*   Updated: 2025/10/30 17:27:42 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/10/30 18:04:46 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+void	pa(t_stack *a, t_stack *b)
+{
+	if (b == NULL)
+		return ;
+
+	
+	ft_printf("pa\n");
+	return ;
+}
+
+void	pb(t_stack *a, t_stack *b)
+{
+	if (a == NULL)
+		return ;
+	
+	ft_printf("pb\n");
+	return ;
+}
--- swap_ab.c
+++ swap_ab.c
@@ -6,16 +6,27 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 17:27:31 by achigvin          #+#    #+#             */
-/*   Updated: 2025/10/30 17:29:28 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/10/30 18:03:45 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	sa(t_stack *a, t_stack *b)
 {
+	if (b == NULL || a == NULL 
+		|| (*a)->next == NULL || (*b)->next == NULL)
+		return ;
+
 	
+	ft_printf("sa\n");
+	return ;
 }
 
 void	sb(t_stack *a, t_stack *b)
 {
+	if (b == NULL || a == NULL 
+		|| (*a)->next == NULL || (*b)->next == NULL)
+		return ;
 	
+	ft_printf("sb\n");
+	return ;
 }
