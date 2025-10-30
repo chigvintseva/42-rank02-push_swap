186861adb9cdf6dd4b41826fc2fd328d8ccf9cae
chigvintseva <al.chigvintseva@gmail.com>
2025-10-30 18:56:25 +0100

    swaps did incorrectly, now they swap within the stack

--- swap_ab.c
+++ swap_ab.c
@@ -6,44 +6,55 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 17:27:31 by achigvin          #+#    #+#             */
-/*   Updated: 2025/10/30 18:40:02 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/10/30 18:56:04 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.c"
 
-void	sa(t_stack *a, t_stack *b)
+void	sa(t_stack **a)
 {
-	t_stack	*a_top_old;
-	t_stack	*b_top_old;
+	t_stack	*old_node_1;
+	t_stack	*old_node_2;
+	t_stack	*node_3;
 
-	if (b == NULL || a == NULL 
-		|| (*a)->next == NULL || (*b)->next == NULL)
+	if (*a == NULL || (*a)->next == NULL)
 		return ;
-	a = a_top_old;
-	b = b_top_old;
-	a = b_top_old;
-	b = a_top_old;
-	a->next = a_top_old->next;
-	b->next = b_top_old->next;
+	old_node_1 = *a;
+	old_node_2 = **a;
+	node_3 = (**a)->next;
+	*a = old_node_2;
+	old_node_2->next = old_node_1;
+	old_node_1->next = node_3;
 	ft_printf("sa\n");
 	return ;
 }
 
-void	sb(t_stack *a, t_stack *b)
+void	sb(t_stack **b);
 {
-	t_stack	*a_top_old;
-	t_stack	*b_top_old;
+	t_stack	*old_node_1;
+	t_stack	*old_node_2;
+	t_stack	*node_3;
 
-	if (b == NULL || a == NULL 
-		|| (*a)->next == NULL || (*b)->next == NULL)
+	if (*b == NULL || (*b)->next == NULL)
 		return ;
-	a = a_top_old;
-	b = b_top_old;
-	a = b_top_old;
-	b = a_top_old;
-	a->next = a_top_old->next;
-	b->next = b_top_old->next;
+	old_node_1 = *b;
+	old_node_2 = **b;
+	node_3 = (**b)->next;
+	*b = old_node_2;
+	old_node_2->next = old_node_1;
+	old_node_1->next = node_3;
 	ft_printf("sb\n");
+	return ;
+}
+
+void	ss(t_stack **a, t_stack **b)
+{
+	if (*a == NULL || (*a)->next == NULL 
+		|| *b == NULL || (*b)->next == NULL)
+		return ;
+	sa(a);
+	sb(b);
+	ft_printf("ss\n");
 	return ;
 }
