0ade1ffd3b7fe191f20b514272edaf52556a6321
chigvintseva <al.chigvintseva@gmail.com>
2025-10-30 18:40:23 +0100

    sa sb r done, not tested

--- push_ab.c
+++ push_ab.c
@@ -6,38 +6,39 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 17:27:41 by achigvin          #+#    #+#             */
-/*   Updated: 2025/10/30 18:27:43 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/10/30 18:34:23 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.c"
 
 void	pa(t_stack *a, t_stack *b)
 {
-	t_stack	*b_top_node;
-	t_stack	*a_top_node;
+	t_stack	*b_top_old;
+	t_stack	*a_top_old;
 
 	if (b == NULL)
 		return ;
-	b_top_node = b;
-	a_top_node = a;
+	b_top_old = b;
+	a_top_old = a;
 	b = b->next;
-	a = b_top_node;
-	b_top_node->next = a_top_node->next;
+	a = b_top_old;
+	b_top_old->next = a_top_old->next;
 	ft_printf("pa\n");
 	return ;
 }
 
 void	pb(t_stack *a, t_stack *b)
 {
-	t_stack	*a_top_node;
-	t_stack	*b_top_node;
+	t_stack	*a_top_old;
+	t_stack	*b_top_old;
 
 	if (a == NULL)
 		return ;
-	a_top_node = a;
-	b_top_node = b;
-	b = a_top_node;
-	a_top_node->next = b_top_node->next;
+	a_top_old = a;
+	b_top_old = b;
+	b = a_top_old;
+	a_top_old->next = b_top_old->next;
 	a = a->next;
 	ft_printf("pb\n");
 	return ;
--- swap_ab.c
+++ swap_ab.c
@@ -6,27 +6,44 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 17:27:31 by achigvin          #+#    #+#             */
-/*   Updated: 2025/10/30 18:03:45 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/10/30 18:40:02 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.c"
+
 void	sa(t_stack *a, t_stack *b)
 {
+	t_stack	*a_top_old;
+	t_stack	*b_top_old;
+
 	if (b == NULL || a == NULL 
 		|| (*a)->next == NULL || (*b)->next == NULL)
 		return ;
-
-	
+	a = a_top_old;
+	b = b_top_old;
+	a = b_top_old;
+	b = a_top_old;
+	a->next = a_top_old->next;
+	b->next = b_top_old->next;
 	ft_printf("sa\n");
 	return ;
 }
 
 void	sb(t_stack *a, t_stack *b)
 {
+	t_stack	*a_top_old;
+	t_stack	*b_top_old;
+
 	if (b == NULL || a == NULL 
 		|| (*a)->next == NULL || (*b)->next == NULL)
 		return ;
-	
+	a = a_top_old;
+	b = b_top_old;
+	a = b_top_old;
+	b = a_top_old;
+	a->next = a_top_old->next;
+	b->next = b_top_old->next;
 	ft_printf("sb\n");
 	return ;
 }
