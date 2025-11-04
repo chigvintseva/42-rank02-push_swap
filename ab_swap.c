61d882a7b9de2f47041c989e591ac54fcbedec1d
chigvintseva <al.chigvintseva@gmail.com>
2025-11-04 16:29:50 +0100

    fixed the printing issue, create one swapping functions called separately fot sa and sb

--- ab_swap.c
+++ ab_swap.c
@@ -6,44 +6,40 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 17:27:31 by achigvin          #+#    #+#             */
-/*   Updated: 2025/11/04 16:22:25 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/11/04 16:29:22 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.c"
 
-void	sa(t_stack **a)
+void	swap_top2(t_stack **to_top)
 {
 	t_stack	*old_node_1;
 	t_stack	*old_node_2;
 	t_stack	*node_3;
 
-	if (*a == NULL || (*a)->next == NULL)
-		return ;
-	old_node_1 = *a;
+	old_node_1 = *to_top;
 	old_node_2 = old_node_1->next;
 	node_3 = old_node_2->next;
-	*a = old_node_2;
+	*to_top = old_node_2;
 	old_node_2->next = old_node_1;
 	old_node_1->next = node_3;
+}
+
+void	sa(t_stack **a)
+{
+	if (*a == NULL || (*a)->next == NULL)
+		return ;
+	swap_top2(a);
 	ft_printf("sa\n");
 	return ;
 }
 
 void	sb(t_stack **b);
 {
-	t_stack	*old_node_1;
-	t_stack	*old_node_2;
-	t_stack	*node_3;
-
 	if (*b == NULL || (*b)->next == NULL)
 		return ;
-	old_node_1 = *b;
-	old_node_2 = old_node_1->next;
-	node_3 = old_node_2->next;
-	*b = old_node_2;
-	old_node_2->next = old_node_1;
-	old_node_1->next = node_3;
+	swap_top2(b);
 	ft_printf("sb\n");
 	return ;
 }
@@ -53,9 +49,8 @@ void	ss(t_stack **a, t_stack **b)
 	if (*a == NULL || (*a)->next == NULL 
 		|| *b == NULL || (*b)->next == NULL)
 		return ;
-	sa(a);
-	sb(b);
-	// will print too many times
+	swap_top2(a);
+	swap_top2(b);
 	ft_printf("ss\n");
 	return ;
 }
