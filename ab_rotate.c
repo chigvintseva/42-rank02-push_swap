4ccd4609b4ee6a23308d5a80f7becdbd2e8db735
chigvintseva <al.chigvintseva@gmail.com>
2025-11-04 16:22:38 +0100

    issue wth action priting in sb ss sa

--- ab_rotate.c
+++ ab_rotate.c
@@ -1,12 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rotate_ab.c                                        :+:      :+:    :+:   */
+/*   ab_rotate.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 17:27:51 by achigvin          #+#    #+#             */
-/*   Updated: 2025/10/30 17:27:52 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/11/04 16:20:53 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+void	ra(t_stack **to_a)
+{
+
+	return ;
+}
+
+void	rb(t_stack **to_b)
+{
+	
+	return ;
+}
+
+void	rr(t_stack **to_a, t_stack **to_b)
+{
+	ft_printf("rr\n");
+	return ;
+}
