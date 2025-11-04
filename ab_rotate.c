a38dfba72b8515b7a2a985da3d477f2ad6d431be
chigvintseva <al.chigvintseva@gmail.com>
2025-11-04 18:36:31 +0100

    error in rotate, wrongly visualised, deleted the prototype

--- ab_rotate.c
+++ ab_rotate.c
@@ -6,7 +6,7 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 17:27:51 by achigvin          #+#    #+#             */
-/*   Updated: 2025/11/04 16:30:21 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/11/04 18:36:07 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,31 @@
 
 void	rotate(t_stack **to_top)
 {
-	
+
+	if (*to_top == NULL || (*to_top)->next == NULL)
+		return ;
+
+	return ;
 }
 
 void	ra(t_stack **to_a)
 {
-
-
+	rotate(to_a);
 	ft_printf("ra\n");
 	return ;
 }
 
 void	rb(t_stack **to_b)
 {
-	
+	rotate(to_b);
 	ft_printf("rb\n");
 	return ;
 }
 
 void	rr(t_stack **to_a, t_stack **to_b)
 {
+	rotate(to_a);
+	rotate(to_b);
 	ft_printf("rr\n");
 	return ;
 }
