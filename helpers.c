03394b96fd861500021cabac037234d761396098
alena <141341998+chigvintseva@users.noreply.github.com>
2025-11-08 00:03:50 +0100

    min ind length functions r done

--- helpers.c
+++ helpers.c
@@ -1,15 +1,62 @@
 
 #include "push_swap.h"
 
-size_t	find_min(t_stack *a)
+size_t	stack_length(t_stack *a)
 {
 	size_t	i;
-	int		number;
+
+	i = 1;
+	while (a->next != NULL)
+	{
+		i++;
+		a = a->next;
+	}
+	return (i);
+}
+
+int	stack_is_sorted(t_stack *a)
+{
+	size_t	index;
+	size_t	length;
+
+	length = stack_length(a);
+	index = 0;
+	while (a->next != NULL && (a->value < a->next->value))
+	{
+		index++;
+		a = a->next;
+	}
+	
+}
+
+int	find_min(t_stack *a)
+{
+	int	number;
 
 	number = a->value;
 	while (a->next != NULL)
 	{
 		if (a->value < number)
-			number = 
+			number = a->value;
+		a = a->next;
 	}
+	if (a->value < number)
+		number = a->value;
+	return (number);
 }
+
+size_t	find_min_index(t_stack *a, int min_value)
+{
+	size_t	index;
+
+	index = 0;
+	while (a->value != min_value && a->next != NULL)
+	{
+		index++;
+		a = a->next;
+	}
+	if (a->next == NULL && a->value == min_value)
+		index++;
+	return (index);
+}
+
